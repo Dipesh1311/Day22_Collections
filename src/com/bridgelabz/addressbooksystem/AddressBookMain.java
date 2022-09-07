32ce5c23bb5281c384cc3fc3c2eb4d5d2170da38
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-09-07 22:53:17 +0530

    Added all the use cases of address book of day9.

---
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbooksystem;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    static Scanner scannerObject = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+            System.out.println("---------- Welcome To Address Book Program!! ----------");
+            AddressBook addressBook = new AddressBook();
+            addressBook.operation();
+        }
+}
